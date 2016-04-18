b4e0568d113e4fb3ef35747819db97676dc97563
SHvsMK <SHvsMK@gmail.com>
2016-04-18 13:41:28 -0500

    d

--- app/cupid/Javascript/Match.js
+++ app/cupid/Javascript/Match.js
@@ -104,10 +104,10 @@ $(document).ready(function(){
   }
 
   function SubmitMatchVote(yes) {
-    if (ranOutOfMatches){
-      alert("You've ran out of matches! Ask your friend to download Cupid to have more potential matches");
-      return;
-    }
+    // if (ranOutOfMatches){
+    //   alert("You've ran out of matches! Ask your friend to download Cupid to have more potential matches");
+    //   return;
+    // }
     usr_data = {'user_id': 1, 'match_id': match_id, 'yes': yes};
     usr_data = JSON.stringify(usr_data);
     $.ajax({
@@ -159,7 +159,7 @@ $(document).ready(function(){
     }else{
       // if we ran out of matches
       ranOutOfMatches=true;
-      alert("You've ran out of matches! Ask your friend to download Cupid to have more potential matches");
+      // alert("You've ran out of matches! Ask your friend to download Cupid to have more potential matches");
     }
 
 
